33aa2fb822ec7079eeb1083c17917f68a4e2cd03
NikolajX4000 <supernikolaj@hotmail.com>
2019-06-17 10:38:32 +0200

    ModelCheck.py

---
+++ ModelCheck.py
@@ -0,0 +1,17 @@
+import numpy as np
+import tensorflow as tf
+# import Card
+
+def getHand(found_cards):
+    text = str(len(found_cards)) + "/5 cards found"
+    model = tf.keras.models.load_model("models/hupra_desktop_model_100.h5")
+
+    if len(found_cards) >= 5:
+        nums = [card.num for card in found_cards[:5]]
+        suits = [card.suit for card in found_cards[:5]]
+        arr = [[nums + suits]]
+
+        num_dict = {0:"Nothing",1:"Pair",2:"Two Pair",3:"Three of a kind",4:"Straight",5:"Flush",6:"Full House",7:"Four of a kind",8:"Straight Flush",9:"Royal Flush"}
+
+        prediction = np.argmax(model.predict_proba(arr))
+        return num_dict[prediction]
